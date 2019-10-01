125c8fc41e85002da151efdf6574a16bf7aaef49
Egor <znogoud3@gmail.com>
2019-10-01 14:58:12 +0300

    fix: make the code more simple

--- src/index.js
+++ src/index.js
@@ -2,23 +2,15 @@ module.exports = function check(str, bracketsConfig) {
   let arr = str.split('');
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < bracketsConfig.length; j++) {
-      if (arr[i] == bracketsConfig[j][0] &&
-        arr[i + 1] == bracketsConfig[j][1]) {
+      if (arr[i] === bracketsConfig[j][0] &&
+        arr[i + 1] === bracketsConfig[j][1]) {
         arr.splice(i, 2);
-        i = 0;
+        i = -1;
+        break;
       }
     }
   }
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < bracketsConfig.length; j++) {
-      if (arr[i] == bracketsConfig[j][0] &&
-        arr[i + 1] == bracketsConfig[j][1]) {
-        arr.splice(i, 2);
-        i = 0;
-      }
-    }
-  }
-  return arr.length == 0;
+  return arr.length === 0;
 }
 
 /*
